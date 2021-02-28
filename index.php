ed382357d312e24628ccd7d42dc7b9898090ab38
JadeXTGitHub <78270947+JadeXTGitHub@users.noreply.github.com>
2021-02-28 08:19:53 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <Title>STIM 3320 Assignment Chapter 6</Title>
+  <meta charset="utf-8">
+
+<style>
+.flex-box {
+  display: flex;
+  background-color: grey;
+}
+
+.flex-box > div {
+  background-color: #111111;
+  margin: 10px;
+  padding: 20px;
+  font-size: 30px;
+  margin: auto;
+  color: White
+}
+.center {
+  margin: auto;
+  width: 33%;
+  padding: 75px;
+}
+
+.footer {
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  color: rgb(122, 118, 118);
+  text-align: center;
+}
+</style>
+
+
+
+
+</head>
+<body>
+
+<div class="flex-box">
+  <div>Contrast</div>
+  <div>Repitition</div>
+  <div>Alignment</div>  
+  <div>Proximity</div>
+</div>
+
+
+<div class="center">
+<p>A Flexible Layout must have a parent element with the <em>display</em> property set to <em>flex</em>.</p>
+<p>The Flexible Box Layout Module, makes it easier to design flexible responsive layout structure without using float or positioning.</p>
+<p>Direct child elements(s) of the flexible container automatically becomes flexible items.</p>
+<p>The webpage I used to learn more about CSS page layout techniques is <a href="https://www.w3schools.com/css/css3_flexbox.asp"> W3 Schools</a> </p>
+<p>The CSS Flexbox Layout design technique follows the principals of design technique:</p>
+
+<li>Contrast - Flexbox layout allows for any colors to be used to create contrast</li>
+<li>Repetition - Flexbox layout allows for repetition and consistancy</li>
+<li>Alignment - Flexbox layout allows for box like alignment</li>
+<li>Proximity - Flexbox layout allows for spacing of boxes</li>
+</div>
+
+
+<div class="footer">
+  <p>STIM 3320 - Jennifer Thomas - DBU Chapter 6 Assignment</p>
+</div>
+</body>
+</html>
